388a63a2ea54e769638934359913f210d3127c7b
Raja Asim <98418439+rajaasim4@users.noreply.github.com>
2023-01-02 15:39:58 +0500

    J Query Issues Fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -319,7 +319,7 @@
       $w = jQuery(window),
       viewTop = $w.scrollTop(),
       viewBottom = viewTop + $w.height(),
-      _top = $t.offset().top,
+      _top = $t.offset()?.top,
       _bottom = _top + $t.height(),
       compareTop = partial === true ? _bottom : _top,
       compareBottom = partial === true ? _top : _bottom;
